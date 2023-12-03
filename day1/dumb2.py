cfc87b9edc0c88e04eff593e7693e3be229370c6
Ertyus Suytre <ertyussuytre@gmail.com>
2023-12-03 04:27:58 -0500

    3 lines

--- day1/dumb2.py
+++ day1/dumb2.py
@@ -1,1 +1,5 @@
-# TODO: can't really substitute words to digits in one line
+import re
+wordtodigit = { 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9',}
+# TODO: get rid of above somehow?!?!?
+
+print(sum([int((re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[0] if re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[0].isdigit() else wordtodigit[re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[0]]) + (re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[-1] if re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[-1].isdigit() else wordtodigit[re.findall(f"(?=(\\d|{'|'.join(wordtodigit.keys())}))", line)[-1]])) for line in open('input.txt').readlines()]))
