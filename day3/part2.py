2f474d03648d64fbc7ad9c0ce23c375221bbadcc
Ertyus Suytre <ertyussuytre@gmail.com>
2023-12-03 01:33:30 -0500

    this is technically better ok

--- day3/part2.py
+++ day3/part2.py
@@ -7,16 +7,27 @@ NEIGHBORS = [
     (1, -1),  (1, 0),  (1, 1)
 ]
 
-def get_num (line, col):
+def get_num (grid, row, col):
     # Get first digit
-    while col - 1 >= 0 and line[col - 1].isdecimal():
+    while col - 1 >= 0 and grid[row][col - 1].isdecimal():
         col -= 1
+        
+    # Get line before the start of number
+    start_row = grid[row][:col]
+    
+    # Get number
     num_str = ''
-    while col < len(line) and line[col].isdecimal():
-        num_str += line[col]
-        line = line[:col] + '.' + line[col+1:]
+    while col < len(grid[row]) and grid[row][col].isdecimal():
+        num_str += grid[row][col]
         col += 1
-    return line, int(num_str)
+
+    # Get line after the end of number
+    end_row = grid[row][col:]
+    
+    # Replace number with periods
+    grid[row] = start_row + '.'*len(num_str) + end_row
+    
+    return int(num_str)
 
 total_sum = 0
 
@@ -25,13 +36,12 @@ for row, line in enumerate(lines):
     for col, char in enumerate(line):
         if char == '*':
             num_numbers = []
-            # Check 8 neighbors for digits
+            # Check 8 neighbors for numbers and record them
             for dx, dy in NEIGHBORS:
                 if 0 <= row + dx < len(lines) and 0 <= col + dy < len(line) and lines[row + dx][col + dy].isdecimal():
-                    line, num = get_num(lines[row + dx], col + dy)
-                    lines[row + dx] = line
+                    num = get_num(lines, row + dx, col + dy)
                     num_numbers.append(num)
-            
+            # If 2 numbers were found, multiply them, and add them to the total
             if len(num_numbers) == 2:
                 total_sum += num_numbers[0] * num_numbers[1]
 
