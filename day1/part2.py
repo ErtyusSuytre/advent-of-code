aa78bef55de41a226ec3c6c09c00bc82fb4c7c2a
Ertyus Suytre <ertyussuytre@gmail.com>
2023-12-04 05:05:12 -0500

    added cleaner regex solution

--- day1/part2.py
+++ day1/part2.py
@@ -52,3 +52,35 @@ for line in lines:
     sum += int(first + last)
 
 print(sum)
+
+import re
+
+wordtodigit = {
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+regex_pattern = re.compile('(?=(' + '|'.join(wordtodigit.keys()) + '))')
+
+sum = 0
+for line in lines:
+    nums = regex_pattern.findall(line)
+    sum += int(wordtodigit[nums[0]] + wordtodigit[nums[-1]])
+
+print(sum)
